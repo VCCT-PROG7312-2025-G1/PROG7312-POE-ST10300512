48e15e83d2dc57b63b428eb0eeacf6cc462e41a9
st10300512 <st10300512@vcconnect.edu.za>
2025-11-12 17:36:04 +0200

    Added new variable and end of file comment

--- PROG7312-POE-ST10300512/Issue.cs
+++ PROG7312-POE-ST10300512/Issue.cs
@@ -8,12 +8,16 @@ namespace PROG7312_POE_ST10300512
 {
     public class Issue
     {
-        //Getters and Setters for Variables
+        //Getters and Setters
         public Guid IssueId { get; set; }
         public string Location { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public List<string> Attachments { get; set; } = new List<string>();
         public DateTime SubmittedDate { get; set; }
+        public int Priority { get; set; }
     }
 }
+
+
+/*----------------|||||||||||||||-------------------END OF FILE----------------|||||||||||||||-------------------*/
