8ab65a708f73e35aede09b9213a2dae5328b1913
st10300512 <st10300512@vcconnect.edu.za>
2025-11-12 17:40:02 +0200

    Getters, setters, constructor and ToString for Part 3

--- PROG7312-POE-ST10300512/ServiceStatus.cs
+++ PROG7312-POE-ST10300512/ServiceStatus.cs
@@ -1,13 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PROG7312_POE_ST10300512
 {
     public class ServiceStatus
-    {
-       
+    {   //getters and setters
+        public string UniqueID { get; private set; }
+        public string ResidentName { get; set; }
+        public string IssueDescription { get; set; }
+        public DateTime DateReported { get; private set; }
+        public string CurrentStatus { get; set; }
+        public int Priority { get; set; } // Lower the number the higher the priority
+
+        // constructor
+        public ServiceStatus(Issue issue)
+        {
+            UniqueID = issue.IssueId.ToString();
+            ResidentName = $"Resident at {issue.Location}";
+            IssueDescription = $"({issue.Category}) {issue.Description}";
+            DateReported = issue.SubmittedDate;
+            CurrentStatus = "Submitted";
+            Priority = issue.Priority;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {UniqueID} ({IssueDescription})";
+        }
     }
 }
